d7dca4eecf0d2fbdbfd8d95dee6157221f2fa634
PoissonPoisson <romain.delorme-glorieux@sfr.fr>
2019-10-22 10:11:52 +0200

    Added ITI.SusanooQuest.sln.

---
+++ ITI.SusanooQuest.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITI.SusanooQuest.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
