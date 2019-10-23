7c27ba911ed004ee4d362647bde4436e59c23bd5
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-10-23 09:24:13 +0200

    Je sais pas

---
+++ ITI.SusanooQuest.Lib/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.SusanooQuest.Lib
+{
+    abstract class Entity
+    {
+        double _length;
+        double _speed = double.MinValue;
+        Vector _pos;
+        Game _game;
+        ushort _live;
+
+        protected Entity (Vector pos, double length, Game game, ushort live)
+        {
+            _length = length;
+            _game = game;
+            _pos = pos;
+            _live = live;
+        }
+
+        internal abstract void Update();
+    }
+}
---
+++ ITI.SusanooQuest.Lib/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.SusanooQuest.Lib
+{
+    class Player : Entity
+    {
+        
+        
+        ushort _bombes = 3;
+        
+        
+        public Player (Vector pos, double length, Game game)
+            : base(pos, length, game, 3)
+        {
+        }
+
+        internal override void Update()
+        {
+
+        }
+    }
+}
