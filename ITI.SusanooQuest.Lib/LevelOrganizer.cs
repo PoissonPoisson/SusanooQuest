a1598126048decf487c08e7f2c0dfddb21f95fa1
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2019-12-11 14:38:15 +0100

    level finalisation

--- ITI.SusanooQuest.Lib/LevelOrganizer.cs
+++ ITI.SusanooQuest.Lib/LevelOrganizer.cs
@@ -29,7 +29,29 @@ namespace ITI.SusanooQuest.Lib
 
         public void LevelOne()
         {
-            if (_context.Ennemy.Count() == 0) VagueOne() ;           
+            switch (_i)
+            {
+                case 0:
+                    if (_context.Ennemy.Count() == 0) VagueOne();
+                    _i++;
+                    break;
+                case 1:
+                    if (_context.Ennemy.Count() == 0) VagueOne();
+                    _i++;
+                    break;
+                case 2:
+                    if (_context.Ennemy.Count() == 0) VagueTwo();
+                    _i++;
+                    break;
+                case 3:
+                    if (_context.Ennemy.Count() == 0) VagueTwo();
+                    _i++;
+                    break;
+                case 4:
+                    if (_context.Ennemy.Count() == 0) FirstBoss();
+                    _i++;
+                    break;
+            }
         }
 
         private void VagueOne()
@@ -41,5 +63,25 @@ namespace ITI.SusanooQuest.Lib
             _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard"));
             _context.Update();
         }
+        private void VagueTwo()
+        {
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(80, 60), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(70, 50), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(80, 100), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(70, 90), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(60, 80), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(50, 70), 20, _context, 25, 5, "standard"));
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(40, 60), 20, _context, 25, 5, "standard"));
+            _context.Update();
+        }
+        private void FirstBoss()
+        {
+            _ennemiesAlive.Add(_context.CreateEnnemy(new Vector(100, 100), 50, _context, 100, 7, "boss"));
+            _context.Update();
+        }
+
     }
 }
