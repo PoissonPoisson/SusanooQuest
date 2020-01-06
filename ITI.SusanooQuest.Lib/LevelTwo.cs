4ffd4b21b9618de331fb2c96431db46dcad54f03
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2020-01-06 14:12:37 +0100

    creation d'une durée de vague dans le  LevelTwo

--- ITI.SusanooQuest.Lib/LevelTwo.cs
+++ ITI.SusanooQuest.Lib/LevelTwo.cs
@@ -16,21 +16,21 @@ namespace ITI.SusanooQuest.Lib
         public List<Ennemy> Alive => _ennemiesAlive;
 
         public List<Ennemy> Dead => _ennemiesDeath;
-        string _date = System.DateTime.Now.ToShortDateString();
+        DateTime _startExecutionDate = DateTime.Now;
+        
         public LevelTwo(List<Ennemy> ennemiesAlive, List<Ennemy> ennemiesDeath, Game ctx)
         {
             _ennemiesAlive = ennemiesAlive;
             _ennemiesDeath = ennemiesDeath;
             _context = ctx;
         }
+
            
                
 
         public void NextVague()
         {
-            switch (_i)
-            {
-                case 0:
+           
                     if (_context.Ennemy.Count() == 0)
                     {
                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
@@ -40,110 +40,14 @@ namespace ITI.SusanooQuest.Lib
                         _context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard");
                         _context.Update();
                     };
-                    _i++;
-                    break;
-                case 1:
-                    if (_context.Ennemy.Count() == 0)
-                    {
-                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard");
-                        _context.Update();
-                    };
-                    _i++;
-                    break;
-                case 2:
-                    if (_context.Ennemy.Count() == 0)
-                    {
-                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(50, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(40, 60), 20, _context, 25, 5, "standard");
-                        _context.Update();
-                    };
-                    _i++;
-                    break;
-                case 3:
-                    if (_context.Ennemy.Count() == 0)
-                    {
-                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(50, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(40, 60), 20, _context, 25, 5, "standard");
-                        _context.Update();
-                    };
-                    _i++;
-                    break;
-                case 4:
-                    if (_context.Ennemy.Count() == 0) FirstBoss();
-                    _i++;
-                    break;
-                case 5:
-                    if (_context.Ennemy.Count() == 0)
-                    {
-                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 40), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(50, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(40, 60), 20, _context, 25, 5, "standard");
-                        _context.Update();
-                    };
-                    _i++;
-                    break;
-                case 6:
-                    if (_context.Ennemy.Count() == 0)
-                    {
-                         _context.CreateEnnemy(new Vector(100, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(100, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(100, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(100, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(100, 40), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(50, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(40, 100), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 80), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 70), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 60), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 50), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(90, 40), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(80, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(70, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(60, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(50, 90), 20, _context, 25, 5, "standard");
-                         _context.CreateEnnemy(new Vector(40, 90), 20, _context, 25, 5, "standard");
-                        _context.Update();
-                    };
-                    _i++;
-                    break;
-                case 7:
-                    if (_context.Ennemy.Count() == 0) Boss();
-                    _i++;
-                    break;
-                case 8:
-                    if (_context.Ennemy.Count() == 0) NextLevel();
-                    break;
-            }
+            TimeSpan duration = DateTime.Now.Subtract(_startExecutionDate);
+            string _executionTime = string.Format(
+                "Temps d'exécution : {0}h, {0}m, {12}s et {0}ms.",
+                duration.Hours,
+                duration.Minutes,
+                duration.Seconds,
+                duration.Milliseconds);
+            
         }
 
 
